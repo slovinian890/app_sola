0a00cdac953bce9daee1d3f30ef4f6cc92f8ad87
Filip <sinjurfilip@gmail.com>
2026-04-16 20:20:15 +0200

    neki

--- react/Runner/app/modal.tsx
+++ react/Runner/app/modal.tsx
@@ -7,7 +7,6 @@ import { ThemedView } from '@/components/themed-view';
 export default function ModalScreen() {
   return (
     <ThemedView style={styles.container}>
-      //popravi tole!
       <ThemedText type="title">This is a modal</ThemedText>
       <Link href="/" dismissTo style={styles.link}>
         <ThemedText type="link">Go to home screen</ThemedText>
